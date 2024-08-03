c54b04b7d808880eecb6f26eb55eb48bf798f677
Martin Melendez-Treder <martin.melendezt@gmx.at>
2024-08-03 22:58:12 +0200

    Cleanup solution

--- ClaimsBasedAuthorizationApplication/Dtos/RegistrationDto.cs
+++ ClaimsBasedAuthorizationApplication/Dtos/RegistrationDto.cs
@@ -5,6 +5,10 @@ namespace ClaimsBasesAuthorizationApplication.Dtos;
 public class RegistrationDto
 {
     [Required] public string Email { get; set; } = null!;
-    [Required] [Compare(nameof(ConfirmPassword))] public string Password { get; set; } = null!;
+
+    [Required]
+    [Compare(nameof(ConfirmPassword))]
+    public string Password { get; set; } = null!;
+
     [Required] [Compare(nameof(Password))] public string ConfirmPassword { get; set; } = null!;
 }
--- ClaimsBasedAuthorizationApplication/Program.cs
+++ ClaimsBasedAuthorizationApplication/Program.cs
@@ -11,9 +11,11 @@ builder.Services.AddControllersWithViews();
 builder.Services.Configure<RouteOptions>(options => options.LowercaseUrls = true);
 
 // Configure database and authentication-scheme.
-builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddScoped<IPasswordHasher<User>, PasswordHasher<User>>();
-builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme).AddCookie(options => { options.LoginPath = "/auth/login"; });
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(options => { options.LoginPath = "/auth/login"; });
 
 var app = builder.Build();
 
@@ -34,7 +36,7 @@ app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    "default",
+    "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
